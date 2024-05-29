46e38140ac78c5f360c739801a275c4556f3863e
beeseo147 <87863897+beeseo147@users.noreply.github.com>
2024-05-29 19:05:34 +0900

    Triger 로직수정

--- Source/CryptRider/Components/TriggerComponent.cpp
+++ Source/CryptRider/Components/TriggerComponent.cpp
@@ -13,12 +13,19 @@ void UTriggerComponent::BeginPlay()
 {
 	Super::BeginPlay();
 	UE_LOG(LogTemp, Display, TEXT("Trigger Component Alive"));
+
 	OnComponentBeginOverlap.AddDynamic(this, &ThisClass::OnOverlapBegin);
+	OnComponentEndOverlap.AddDynamic(this, &ThisClass::OnOverlapEnd);
 }
 
 void UTriggerComponent::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
 {
-	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);	
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+	AActor* Actor = GetAcceptableActor();
+	if (Actor == nullptr)
+	{
+		Mover->SetbShouldMove(false);
+	}
 }
 
 void UTriggerComponent::SetMover(UMover* NewMover)
@@ -47,8 +54,6 @@ void UTriggerComponent::OnOverlapBegin(UPrimitiveComponent* OverlappedComp, AAct
 		Actor->SetActorRotation(FRotator(this->GetComponentRotation()), ETeleportType::TeleportPhysics);
 
 		Mover->SetbShouldMove(true);
-
-
 	}
 	else
 	{
@@ -57,15 +62,33 @@ void UTriggerComponent::OnOverlapBegin(UPrimitiveComponent* OverlappedComp, AAct
 	}
 }
 
+void UTriggerComponent::OnOverlapEnd(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex)
+{
+	AActor* Actor = GetAcceptableActor();
+	if (Actor != nullptr)
+	{
+		//UnLock
+		UPrimitiveComponent* Component = Cast<UPrimitiveComponent>(Actor->GetRootComponent());
+		if (Component != nullptr)
+		{
+			Component->SetSimulatePhysics(true);
+		}
+		Mover->SetbShouldMove(false);
+	}
+	else
+	{
+		Mover->SetbShouldMove(false);
+	}
+}
+
 AActor* UTriggerComponent::GetAcceptableActor() const
 {
 	TArray<AActor*>Actors;
 	GetOverlappingActors(Actors);
-
 	for (auto Actor : Actors)
 	{
 		bool IsGrabbed = Actor->ActorHasTag("Grabbed");
-		if (Actor->GetActorNameOrLabel().Contains(AcceptableActorTag) && !Actor->ActorHasTag("Grabbed"))
+		if (Actor->GetActorNameOrLabel().Contains(AcceptableActorTag) && !IsGrabbed)
 		{
 			return Actor;
 		}
--- Source/CryptRider/Components/TriggerComponent.h
+++ Source/CryptRider/Components/TriggerComponent.h
@@ -31,7 +31,8 @@ public:
 
 	UFUNCTION()
 	void OnOverlapBegin(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
-
+	UFUNCTION()
+	void OnOverlapEnd(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex);
 private:
 	AActor* GetAcceptableActor() const;
 private:
